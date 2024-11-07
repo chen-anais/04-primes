1a52b7ac5a63b450559c81602279336666a3df59
chen-anais <anais.chen@edu.esiee.fr>
2024-11-07 09:11:05 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
+""" 
+contient une fonction isprime qui retourne si le nombre passé en paramètre est premier
+"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
-    premier = True
-    n = int(input("entrer un nombre: "))
-    max = int(sqrt(n)) + 1
-    for i in range(2,max + 1):
-        if n % i == 0:
-            premier = False
-            a= int(n/i)
-            print(f'{n} = {i} x {a} : False')
-            break
+    """
+    Retourne True si p est premier et False si il ne l'est pas
+    """
+    if p in (0, 1) :
+        return False
+    for i in range(2,int(sqrt(p)) + 1):
+        if p % i == 0:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
+    """
+    Teste si les nombres de 0 à 99 sont premiers
 
+    """
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
